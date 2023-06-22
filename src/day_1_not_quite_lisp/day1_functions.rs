ab5d77efd49952e1aba214c2007275fd72c7e2f8
christoph bothe <christoph_bothe@gmx.de>
2023-06-22 17:19:28 +0200

    clean up

--- src/day_1_not_quite_lisp/day1_functions.rs
+++ src/day_1_not_quite_lisp/day1_functions.rs
@@ -2,35 +2,31 @@ pub mod day1_functions {
 use std::fs;
 
 pub fn read_file(file_path: &str)-> String {
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    let mut buf = String::with_capacity(contents.len());
-    for c in contents.chars() {
-        buf.push(c);
-    }
-
-    return buf;
+  let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+  let mut buf = String::with_capacity(contents.len());
+  for c in contents.chars() {
+    buf.push(c);
+  }
+  return buf;
 }
 
 pub fn solve_day1() {
-    // --snip--
-    let file_path = "/home/geegee/org/roam/Programming/Rust/earthlings_aoc_2015/src/day_1_not_quite_lisp/input.txt";
-    let contents:String = read_file(file_path);
-    let mut santa_floor:i32 = 0;
-    let mut pos:i32 = 0;
-    let mut found:bool = false;
-    for i in contents.chars() {
-        pos += 1;
-        if i == '(' {
-            santa_floor += 1;
-        } else if i == ')' {
-            santa_floor -= 1;}
-        if santa_floor == -1 && !found {
-            print!("First position Santa is in floor -1: {pos} \n");
-            found = true;
-        }
-
-        //        print!("{}", i);
+  let file_path = "src/day_1_not_quite_lisp/input.txt";
+  let contents:String = read_file(file_path);
+  let mut santa_floor:i32 = 0;
+  let mut pos:i32 = 0;
+  let mut found:bool = false;
+  for i in contents.chars() {
+    pos += 1;
+    if i == '(' {
+      santa_floor += 1;
+    } else if i == ')' {
+      santa_floor -= 1;}
+    if santa_floor == -1 && !found {
+      print!("First position Santa is in floor -1: {pos} \n");
+      found = true;
     }
-    print!("Final floor Santa ends up in: {santa_floor}") 
+  }
+  print!("Final floor Santa ends up in: {santa_floor}") 
 }}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-// use std::env;
 pub mod day_1_not_quite_lisp;
 use day_1_not_quite_lisp::day1_functions::day1_functions::solve_day1;
 
 fn main() {
-    // --snip--
-    solve_day1()
+  solve_day1()
 }
