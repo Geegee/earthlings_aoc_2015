3ee0ec141012caeec336e25ddcc2821c6119e8db
geegee <wgolff@googlemail.com>
2023-06-22 09:27:57 +0200

    Solution to the first problem of the AOC 2015 in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// use std::env;
+pub mod day_1_not_quite_lisp;
+use day_1_not_quite_lisp::day1_functions::day1_functions::solve_day1;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    solve_day1()
 }
